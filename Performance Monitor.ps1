f7b5ed7b15fc17e677bb1a01e804998ae87037a0
RonRunnerElowSum <97925118+RonRunnerElowSum@users.noreply.github.com>
2022-02-08 15:42:49 -0500

    Update and rename TEST to Performance Monitor.ps1

---
+++ Performance Monitor.ps1
@@ -0,0 +1,158 @@
+#Warning  utilization percentage is greater than or equal to this amount
+$CPUWarningThreshhold = "90"
+#Warning when this amount or less of RAM is remaining
+$RAMWarningThreshhold = "1"
+#Network interface warning threshhold in MBps (multiply MBps by 8 to get Mbps)
+$NetInterfaceUploadWarningThreshhold = "4"
+$NetInterfaceDownloadWarningThreshhold = "5"
+
+function Write-MSPLog {
+    Param
+    (
+         [Parameter(Mandatory=$true, Position=0)]
+         [ValidateSet('MSP Performance Monitor')]
+         [string] $LogSource,
+         [Parameter(Mandatory=$true, Position=1)]
+         [ValidateSet('Information','Warning','Error')]
+         [string] $LogType,
+         [Parameter(Mandatory=$true, Position=2)]
+         [string] $LogMessage
+    )
+
+    New-EventLog -LogName MSP-IT -Source 'MSP' -ErrorAction SilentlyContinue
+    if(!(Get-EventLog -LogName MSP-IT -Source 'MSP Performance Monitor' -ErrorAction SilentlyContinue)){
+        New-EventLog -LogName MSP-IT -Source 'MSP Performance Monitor' -ErrorAction SilentlyContinue
+    }
+    Write-EventLog -Log MSP-IT -Source "MSP Performance Monitor" -EventID 0 -EntryType $LogType -Message "$LogMessage"
+}
+
+function PostPerformanceData {
+
+    $EndpointSerial = (Get-CimInstance win32_bios).SerialNumber
+    $EndpointComputerName = [System.Net.Dns]::GetHostByName($Env:ComputerName).HostName
+    $EndpointOS = (Get-WmiObject -class Win32_OperatingSystem).Caption
+    if($EndpointOS | Select-String "Server"){
+        $EndpointType = "Server"
+    }
+    else{
+        $EndpointType = "Workstation"
+    }
+    if(($Null -eq $EndpointSerial) -or ($EndpointSerial -eq "To be filled by O.E.M.")){
+        $EndpointSerial = $EndpointComputerName
+    }
+    if(Test-Path "C:\ComSys\EndpointSiteName.txt" -ErrorAction SilentlyContinue){
+        $EndpointSiteName = Get-Content -Path "C:\ComSys\EndpointSiteName.txt"
+    }
+    elseif(Test-Path "C:\Program Files\SAAZOD\ApplicationLog\zSCCLog\zDCMGetSitecode.log" -ErrorAction SilentlyContinue){
+        $EndpointSiteName = (Get-Content -Path "C:\Program Files\SAAZOD\ApplicationLog\zSCCLog\zDCMGetSitecode.log" | Select-String "sitename=" | Select-Object -Last 1) -Replace "^.*?="
+    }
+    elseif(Test-Path "C:\Program Files (x86)\SAAZOD\ApplicationLog\zSCCLog\zDCMGetSitecode.log" -ErrorAction SilentlyContinue){
+        $EndpointSiteName = (Get-Content -Path "C:\Program Files (x86)\SAAZOD\ApplicationLog\zSCCLog\zDCMGetSitecode.log" | Select-String "sitename=" | Select-Object -Last 1) -Replace "^.*?="
+    }
+    if($Null -eq $EndpointSiteName){
+        if(Test-Path "C:\Program Files\SAAZOD\ApplicationLog\WebPost\WebPostDLL\WebPostComp-tfr_wpdcmgetsitecode.Log" -ErrorAction SilentlyContinue){
+            $EndpointSiteName = (((((Get-Content -Path "C:\Program Files\SAAZOD\ApplicationLog\WebPost\WebPostDLL\WebPostComp-tfr_wpdcmgetsitecode.Log" | Select-String "<sitename>" | Select-Object -Last 1) -Replace "^.*?!") -Replace "^.*?!") -Replace "^.*?!") -Replace '\[CDATA\[','') -Split '\]\]'[-1] | Select-Object -First 1
+        }
+        if(Test-Path "C:\Program Files (x86)\SAAZOD\ApplicationLog\WebPost\WebPostDLL\WebPostComp-tfr_wpdcmgetsitecode.Log" -ErrorAction SilentlyContinue){
+            $EndpointSiteName = (((((Get-Content -Path "C:\Program Files (x86)\SAAZOD\ApplicationLog\WebPost\WebPostDLL\WebPostComp-tfr_wpdcmgetsitecode.Log" | Select-String "<sitename>" | Select-Object -Last 1) -Replace "^.*?!") -Replace "^.*?!") -Replace "^.*?!") -Replace '\[CDATA\[','') -Split '\]\]'[-1] | Select-Object -First 1
+        }
+    }
+
+    try{
+        if(!(Test-Path -Path "C:\MSP\secret.txt")){
+            Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Error" -LogMessage "C:\MSP\secret.txt does not exist...exiting..."
+            EXIT
+        }
+        $SensitiveString = Get-Content -Path "C:\MSP\secret.txt" | ConvertTo-SecureString
+        $Marshal = [System.Runtime.InteropServices.Marshal]
+        $Bstr = $Marshal::SecureStringToBSTR($SensitiveString)
+        $DecryptedString = $Marshal::PtrToStringAuto($Bstr)
+        $Marshal::ZeroFreeBSTR($Bstr)
+        $SQLServer = $DecryptedString -split ";" | Select-Object -Index 0
+        $SQLDatabase = $DecryptedString -split ";" | Select-Object -Index 1
+        $SQLUsername = $DecryptedString -split ";" | Select-Object -Index 2
+        $SQLPassword = $DecryptedString -split ";" | Select-Object -Index 3
+    }
+    catch{
+        Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Error" -LogMessage "Failed to decrypt SQL connection info...exiting..."
+    }
+
+    $DateTime = Get-Date -Format "MM/dd/yyyy HH:mm"
+    Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Information" -LogMessage "Posting the following information:`r`n`r`n"
+
+$SQLCommand = @"
+if exists(SELECT * from Table_CustomerPerformanceData where EndpointSerial='$EndpointSerial')
+BEGIN            
+UPDATE Table_CustomerPerformanceData SET EndpointComputerName='$EndpointComputerName',EndpointOS='$EndpointOS',EndpointType='$EndpointType',EndpointSiteName='$EndpointSiteName',EndpointCPUStatus='$EndpointCPUStatus',EndpointCPUUtilization='$CPUUtilization',EndpointRAMStatus='$EndpointRAMStatus',EndpointAvailableRAMInGB='$AvailableRAM',EndpointNetInterfaceUploadStatus='$EndpointNetIntUploadStatus',EndpointNetInterfaceUploadUtilizationInMBps='$NetInterfaceUploadUtilizationInMBps',EndpointNetInterfaceDownloadStatus='$EndpointNetIntDownloadStatus',EndpointNetInterfaceDownloadUtilizationInMBps='$NetInterfaceDownloadUtilizationInMBps',LastPostDate='$DateTime' WHERE (EndpointSerial = '$EndpointSerial')
+END                  
+else            
+BEGIN
+INSERT INTO [$SQLDatabase].[dbo].[Table_CustomerPerformanceData](EndpointSerial,EndpointComputerName,EndpointOS,EndpointType,EndpointSiteName,EndpointCPUStatus,EndpointCPUUtilization,EndpointRAMStatus,EndpointAvailableRAMInGB,EndpointNetInterfaceUploadStatus,EndpointNetInterfaceUploadUtilizationInMBps,EndpointNetInterfaceDownloadStatus,EndpointNetInterfaceDownloadUtilizationInMBps,LastPostDate)
+VALUES ('$EndpointSerial','$EndpointComputerName','$EndpointOS','$EndpointType','$EndpointSiteName','$EndpointCPUStatus','$CPUUtilization','$EndpointRAMStatus','$AvailableRAM','$EndpointNetIntUploadStatus','$NetInterfaceUploadUtilizationInMBps','$EndpointNetIntDownloadStatus','$NetInterfaceDownloadUtilizationInMBps','$DateTime')
+END
+"@      
+
+    $Params = @{
+        'ServerInstance'=$SQLServer;
+        'Database'=$SQLDatabase;
+        'Username'=$SQLUsername;
+        'Password'=$SQLPassword
+    }
+    
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-SqlCmd @Params -Query $SQLCommand -EncryptConnection
+}
+
+function Get-CPUUtilization {
+    (Get-Counter -Counter '\processor(_total)\% processor time').CounterSamples.CookedValue
+}
+
+function Get-AvailableRAM {
+    [math]::Round((Get-Counter -Counter '\*Memory\Available Bytes').CounterSamples.CookedValue / 1GB,1)
+}
+
+if(!(Test-Path -Path "C:\MSP\secret.txt")){
+    Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Error" -LogMessage "C:\MSP\secret.txt does not exist...exiting..."
+    EXIT
+}
+
+do{
+    [int]$CPUUtilization = Get-CPUUtilization
+    [int]$AvailableRAM = Get-AvailableRAM
+    [int]$NetInterfaceUploadUtilizationInMBps = (Get-Counter -Counter "\Network interface(*)\Bytes sent/sec").CounterSamples.CookedValue / 1MB
+    [int]$NetInterfaceDownloadUtilizationInMBps = (Get-Counter -Counter "\Network interface(*)\Bytes received/sec").CounterSamples.CookedValue / 1MB
+    if($CPUUtilization -ge $CPUWarningThreshhold){
+        $EndpointCPUStatus = "Warning: High Utilization"
+        Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Warning" -LogMessage "$Env:ComputerName's CPU utilization has peaked $CPUWarningThreshhold% at $(Get-Date)"
+        #$Top10Processes = (Get-Counter -Counter "\Process(*)\% Processor Time").CounterSamples | Select-Object -First 10 | Sort-Object -Property CookedValue -Descending | Format-Table -Property InstanceName, CookedValue -AutoSize
+    }
+    else{
+        $EndpointCPUStatus = "Healthy"
+    }
+    if($AvailableRAM -le $RAMWarningThreshhold){
+        $EndpointRAMStatus = "Warning: High Utilization"
+        Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Warning" -LogMessage "$Env:ComputerName's available RAM has gone below $RAMWarningThreshhold GB at $(Get-Date)"
+    }
+    else{
+        $EndpointRAMStatus = "Healthy"
+    }
+    if($NetInterfaceUploadUtilizationInMBps -ge $NetInterfaceUploadWarningThreshhold){
+        $EndpointNetIntUploadStatus = "Warning: High Upload Utilization"
+        Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Warning" -LogMessage "$Env:ComputerName's network interface upload/send utilizations has peaked $NetInterfaceUploadWarningThreshhold MBps at $(Get-Date)"
+    }
+    else{
+        $EndpointNetIntUploadStatus = "Healthy"
+    }
+    if($NetInterfaceDownloadUtilizationInMBps -ge $NetInterfaceDownloadWarningThreshhold){
+        $EndpointNetIntDownloadStatus = "Warning: High Upload Utilization"
+        Write-MSPLog -LogSource "MSP Performance Monitor" -LogType "Warning" -LogMessage "$Env:ComputerName's network interface download/receive utilizations has peaked $NetInterfaceDownloadWarningThreshhold MBps at $(Get-Date)"
+    }
+    else{
+        $EndpointNetIntDownloadStatus = "Healthy"
+    }
+    PostPerformanceData
+    Start-Sleep -Seconds 5
+}
+while(
+    $True
+)
